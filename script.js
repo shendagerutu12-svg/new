9e4ca1850d9d9393466902912e539306d43a1323
shendagerutu12-svg <shendagerutu12@gmail.com>
2026-01-08 18:50:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//let Age=20;
+//console.log(Age) ....number
+
+//let condition = (55 > 57); .... boolean
+
+//let city = 'Tasgaon';.... string
+
+//let x;    undefined
+
+//let y = null;   ...null
+//-----operator ....
+ //let a=20;
+ //let b=10;
+// let c= a+b;
+//let d= a-b;
+// let e= a*b;
+ //let f= a/b; .....arithmetic
+ 
+ 
+ //let a=(5>3);
+ //let b=(5==5);
+// let c=(5=="4");......comparision operators
+
+//let a=(5>3)&&(2>3); ....logical operator
+
+//let a = 5;
+//a += 3; ...assignment operator
+
+//let x = Number("5");  // converts string "5" to number 5
+//let y = String(10);   // converts 10 to "10"
+//let z = Boolean(0);   // converts to false......type conversion
+
+
+let result=(5>2 ? "yes":"no");// ternary operator
+
+console.log(result);
+
+
+
+
+
